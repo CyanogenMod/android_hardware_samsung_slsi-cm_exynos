5f53dc25e6edf080dea6dc296c9c006031ff452c
Christopher N. Hesse <raymanfx@gmail.com>
2015-07-10 15:42:21 +0200

    videocodec: Add support for exynos5433
    Change-Id: Iebd77215ed30e83de6ca90b3b46f0ccdbc7f4b76

--- libvideocodec/Android.mk
+++ libvideocodec/Android.mk
@@ -38,6 +38,10 @@ ifeq ($(TARGET_SOC), exynos5430)
 LOCAL_CFLAGS += -DSOC_EXYNOS5430
 endif
 
+ifeq ($(TARGET_SOC), exynos5433)
+LOCAL_CFLAGS += -DSOC_EXYNOS5430
+endif
+
 LOCAL_MODULE := libExynosVideoApi
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false

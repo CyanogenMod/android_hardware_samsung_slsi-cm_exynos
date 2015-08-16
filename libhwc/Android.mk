347c2f3cae0256087a4adfad0e7063430ebaf24c
bmc08gt <brandon.mcansh@gmail.com>
2015-08-16 08:31:14 +0200

    exynos: libhwc: Support multiarch builds
    Change-Id: Idacb697e93b7cc08f3268a78ce21f034a599ff2e
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- libhwc/Android.mk
+++ libhwc/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libGLESv1_CM libhardware \
 	libhardware_legacy libion_exynos libutils libsync \
 	libexynosv4l2 libMcClient libexynosutils libhwcutils libdisplay libhdmi \
